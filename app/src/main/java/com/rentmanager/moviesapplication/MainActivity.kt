ad9fa9179f6fb37e6b44430a94e253eb392b2fe0
Vishal Gupta <vishalgupta@Vishals-MacBook-Pro.local>
2022-07-09 16:20:27 +0530

    add navigation and compose

---
+++ app/src/main/java/com/rentmanager/moviesapplication/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.rentmanager.moviesapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.rentmanager.moviesapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+
+    }
+}
